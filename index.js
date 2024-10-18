2b1cc2951f38e1d67bd2cde8f3c54d4fa8322c82
Bivek Baral <baralbibek1234@gmail.com>
2024-10-18 22:56:31 +0930

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// console.log("Hello world");
+// "abc".toUpperCase()[(1, 2, 3, 4)].push[4];
+// function myFunction(msg) {
+//   console.log("Welcome to Aapna college");
+//   console.log("We are learning JS");
+// }
+// myFunction();
+
+// create a function that is used to add two numbers
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+
+// function sum(x, y) {
+//   s = x + y;
+//   return s;
+// }
+// let val = sum(3, 4);
+// console.log(val);
+function sum(x, y) {
+  z = x + y;
+  return z;
+}
+let val = sum(2, 8);
+console.log(val);
