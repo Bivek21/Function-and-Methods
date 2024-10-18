6619df72f4f237cee4e7eeee9ff9ac7bdd293056
Bivek Baral <baralbibek1234@gmail.com>
2024-10-18 23:07:21 +0930

    arrow function

--- index.js
+++ index.js
@@ -17,9 +17,22 @@
 // }
 // let val = sum(3, 4);
 // console.log(val);
-function sum(x, y) {
-  z = x + y;
-  return z;
-}
-let val = sum(2, 8);
-console.log(val);
+// function sum(x, y) {
+//   z = x + y;
+//   return z;
+// }
+// let val = sum(2, 8);
+// console.log(val);
+//sum function
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// //multiplication function\
+// function multiplication(x, y) {
+//   return x * y;
+// }
+const x = 5;
+const arrowSum = (a, b) => {
+  console.log(a + b);
+};
